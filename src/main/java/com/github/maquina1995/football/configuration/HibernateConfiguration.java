51229385982a8aebf165ad77d98034ff6ad1636c
MaQuiNa1995 <maquina1995@gmail.com>
2021-10-18 23:55:11 +0200

    Añadidas las configuraciones

---
+++ src/main/java/com/github/maquina1995/football/configuration/HibernateConfiguration.java
@@ -0,0 +1,81 @@
+package com.github.maquina1995.football.configuration;
+
+import javax.sql.DataSource;
+
+import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+/**
+ * Configuración de hibernate/base de datos
+ */
+@SpringBootConfiguration
+@EnableTransactionManagement
+public class HibernateConfiguration {
+
+	/**
+	 * Crea el bean encargado de la configuración de base de datos
+	 * 
+	 * @param user {@link String} <code>${database.user:sa}</code> usuario de base
+	 *             de datos
+	 * @param pass {@link String} <code>${database.password}</code> password de base
+	 *             de datos
+	 * @param user {@link String}
+	 *             <code>${database.driver:org.hsqldb.jdbcDriver}</code> driver de
+	 *             base de datos
+	 * @param pass {@link String}
+	 *             <code>${database.url:jdbc:hsqldb:mem:maquina1995}</code> url de
+	 *             base de datos
+	 * @param pass {@link String}
+	 *             <code>${database.poolName:MaQuiNa1995-HikariCP}</code> nombre del
+	 *             pool
+	 * 
+	 * @return {@link HikariDataSource} configurado
+	 */
+	@Bean
+	public DataSource datasource(@Value(value = "${database.user:sa}") String user,
+	        @Value(value = "${database.password:}") String pass,
+	        @Value(value = "${database.driver:org.hsqldb.jdbcDriver}") String driver,
+	        @Value(value = "${database.url:jdbc:hsqldb:mem:maquina1995}") String url,
+	        @Value(value = "${database.poolName:MaQuiNa1995-HikariCP}") String poolName) {
+
+		HikariDataSource dataSource = new HikariDataSource();
+
+		dataSource.setUsername(user);
+		dataSource.setPassword(pass);
+		dataSource.setDriverClassName(driver);
+		dataSource.setJdbcUrl(url);
+
+		dataSource.setMaximumPoolSize(10);
+		dataSource.setPoolName(poolName);
+
+		dataSource.addDataSourceProperty("dataSource.cachePrepStmts", "true");
+		dataSource.addDataSourceProperty("dataSource.prepStmtCacheSize", "250");
+		dataSource.addDataSourceProperty("dataSource.prepStmtCacheSqlLimit", "2048");
+		dataSource.addDataSourceProperty("dataSource.useServerPrepStmts", "true");
+
+		return dataSource;
+	}
+
+	/**
+	 * Crea la {@link Configuration} enlazandole
+	 * {@link CustomPhysicalNamingStrategy} pasado por parámetro
+	 * 
+	 * @param customPhysicalNamingStrategy {@link CustomPhysicalNamingStrategy}
+	 * 
+	 * @return {@link Configuration}
+	 */
+	@Bean
+	public Configuration hibernateConfig(CustomPhysicalNamingStrategy customPhysicalNamingStrategy) {
+
+		Configuration hibernateConfiguration = new Configuration();
+		hibernateConfiguration.setPhysicalNamingStrategy(customPhysicalNamingStrategy);
+
+		return hibernateConfiguration;
+
+	}
+}
