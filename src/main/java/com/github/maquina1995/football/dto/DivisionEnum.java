0e4a59b467f5d68f3f100e7266fb92938d2f2ef8
MaQuiNa1995 <maquina1995@gmail.com>
2021-10-19 00:06:03 +0200

    Añadidos los dto

---
+++ src/main/java/com/github/maquina1995/football/dto/DivisionEnum.java
@@ -0,0 +1,10 @@
+package com.github.maquina1995.football.dto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum DivisionEnum {
+	PRIMERA(1), SEGUNDA(2), TERCERA(3);
+
+	public final int division;
+}
